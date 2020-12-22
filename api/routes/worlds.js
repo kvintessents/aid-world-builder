38f110fc61199abb176057c8699741b7750d9b13
Amiran Gorgazjan <amiran.gorgazjan@tidal.com>
2020-12-22 16:25:37 +0100

    Removed unused code.

--- api/routes/worlds.js
+++ api/routes/worlds.js
@@ -132,22 +132,4 @@ router.post('/world/:id', userAuth, celebrate({
     res.json({ success: true, data: result });
 }));
 
-// router.get('/recreate/worlds', asyncRoute(async function (req, res) {	
-//     await query(`DROP TABLE IF EXISTS worlds`);	
-
-//     const { result, fields } = await query(sql`	
-//         CREATE TABLE IF NOT EXISTS worlds(	
-//             id INT unsigned AUTO_INCREMENT PRIMARY KEY,	
-//             user_id INT NOT NULL,	
-//             is_public TINYINT NOT NULL,	
-//             name VARCHAR(100) NOT NULL,	
-//             document LONGTEXT NOT NULL,	
-//             version BIGINT NOT NULL,	
-//             created_at TIMESTAMP NOT NULL	
-//         );	
-//     `);	
-
-//     res.json({ success: true, data: { result, fields } });	
-// }));
-
 module.exports = router;
